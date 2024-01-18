81663c59a18714c0de617d5f9cc70995cb47555b
Sakshi Rawat <56435752+sakshirawat18@users.noreply.github.com>
2024-01-18 11:36:25 +0530

    made marquee in home page

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import Marquee from "react-fast-marquee";
+
 const Home = () => {
   return (
     <div className="home">
@@ -79,9 +81,19 @@ const Home = () => {
             <br />
             Without the right images - you are losing conversions
           </p>
+          <div className="marqueeContainer">
+          <Marquee>
+            <img src="https://claid.ai/static/3e68c0e0b346cb33e25ae464a8f4ca2b/35aa2/Fruits.webp" alt="food" style={{margin: "0px 30px 120px", width: "512", height: "850px"}} />
+            <img src="https://claid.ai/static/ccbba0087c4dde2b8d41fe49d9dc39b7/35aa2/Potrait.webp" alt="lady" style={{margin: "120px 30px 0px", width: "550px", height: "850px"}}/>
+            <img src="https://claid.ai/static/e75114c66a4d7e8f8e34269918d540af/35aa2/Oil.webp" alt="bottle" style={{margin: "0px 30px 120px", width: "550px", height: "850px"}}/>
+            <img src="https://claid.ai/static/fea351d5354c1e41a2abd7b5c16e09a9/35aa2/Room.webp" alt="bottle" style={{margin: "120px 30px 0px", width: "550px", height: "850px"}}/>
+          </Marquee>
+          </div>
         </div>
       </div>
-      <div className="contactUs"></div>
+      <div className="contactUs">
+
+      </div>
     </div>
   );
 };
