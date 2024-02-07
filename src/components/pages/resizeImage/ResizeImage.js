2eb56aec0321818923c452ec2353aa396b301aed
Sakshi Rawat <sakshirawat@Sakshis-MacBook-Air.local>
2024-02-07 22:31:29 +0530

    started work in resizeImage page

--- src/components/pages/resizeImage/ResizeImage.js
+++ src/components/pages/resizeImage/ResizeImage.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import Header from '../../shared/header/Header'
+import React from "react";
+import Header from "../../shared/header/Header";
+import "./ResizeImage.css";
+import Footer from "../../shared/footer/Footer";
 function ResizeImage() {
   return (
-    <div>
+    <>
       <Header />
-      ResizeImage</div>
-  )
+      <div className="resizeImage">
+        <div className="herosection-resizeImage"></div>
+        <div className="marqueSection"></div>
+        <div className="steps-resizeImage"></div>
+        <div className="getstarted-resizeImage"></div>
+      </div>
+      <Footer />
+    </>
+  );
 }
-export default ResizeImage
+export default ResizeImage;
